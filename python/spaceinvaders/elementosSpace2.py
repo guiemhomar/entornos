c0059019a01d5828dfcaf678302d94314ef9e6fd
guiemhomar <ghomar@nigul.cide.es>
2024-01-09 17:16:12 +0100

    grupo sprites

--- python/spaceinvaders/elementosSpace2.py
+++ python/spaceinvaders/elementosSpace2.py
@@ -1,5 +1,6 @@
 from typing import Any
 import pygame
+from pygame.sprite import Group
 
 
 class Nave(pygame.sprite.Sprite):
@@ -10,11 +11,13 @@ class Nave(pygame.sprite.Sprite):
         self.fotos = [pygame.image.load("balonAntes.png"), pygame.image.load("balonDespues.png")]
         self.indice_image = 0
         self.image = self.fotos[self.indice_image]
+        self.mask = pygame.mask.from_surface(self.image)
         self.contador_fotos = 0
         #creamos un rectangulo a partir de la imagen
         self.rect = self.image.get_rect()
         # actualizr la posicion del rectangulo para que councida con "posicion"
         self.rect.topleft = posicion
+        self.ultimo_disparo = 0
 
     #update
     def update(self, *args: Any, **kwargs: Any) -> None:
@@ -27,11 +30,28 @@ class Nave(pygame.sprite.Sprite):
             self.rect.x = max(0, self.rect.x)
         if teclas[pygame.K_RIGHT]:
             self.rect.x += 2
-            self.rect.x = min(pantalla.get_width()-self.fotos.get_width(), self.rect.x)
+            self.rect.x = min(pantalla.get_width()-self.image.get_width(), self.rect.x)
         #gestionamos la animacion
         self.contador_fotos = (self.contador_fotos + 1) % 40
         self.indice_image = self.contador_fotos // 20
         self.image = self.fotos[self.indice_image]
+        #capturamos grupo_sprites_enemigos
+        grupo_sprites_enemigos = args[3]
+        #capturamos la variable de control running
+        running = args [4]
+        #detectar colisiones con enemigos
+        enemigo_colision = pygame.sprite.spritecollideany(self, grupo_sprites_enemigos, pygame.sprite.collide_mask)
+        if enemigo_colision:
+            enemigo_colision.kill()
+            running[0] = False
+    
+    
+    def disparar(self,grupo_sprites):
+        momento_actual = pygame.time.get_ticks()
+        if momento_actual > self.ultimo_disparo + 200:
+            bala = Bala((self.rect.x + self.image.get_width() / 2, self.rect.y))
+            grupo_sprites.add(bala)
+            self.ultimo_disparo = momento_actual
 
 
 class Enemigo(pygame.sprite.Sprite):
@@ -41,6 +61,7 @@ class Enemigo(pygame.sprite.Sprite):
         #cargamos la imagen
         imagen = pygame.image.load("balonAntes.png")
         self.image = pygame.transform.rotate(imagen, 180)
+        self.mask = pygame.mask.from_surface(self.image)
         #creamos un rectangulo a partir de la imagen
         self.rect = self.image.get_rect()
         # actualizr la posicion del rectangulo para que councida con "posicion"
@@ -48,3 +69,36 @@ class Enemigo(pygame.sprite.Sprite):
 
     def update(self, *args: Any, **kwargs: Any) -> None:
         self.rect.y += 1
+        #capturamos la pantalla
+        pantalla = pygame.display.get_surface()
+        if (self.rect.y > pantalla.get_height()):
+            self.kill()
+    
+class Fondo(pygame.sprite.Sprite):
+    def __init__(self) -> None:
+        #cargamos la imagen
+        imagen = pygame.image.load("campo de futbol.png")
+        #capturamos la pantalla
+        pantalla = pygame.display.get_surface()
+        self.image = pygame.transform.scale(imagen, (pantalla.get_width(), imagen.get_height()))
+        #creamos un rectangulo a partir de la imagen
+        self.rect = self.image.get_rect()
+        # actualizr la posicion del rectangulo para que councida con "posicion"
+        self.rect.topleft = (0,0)
+
+    def update(self, *args, **kwargs):
+        pass
+
+class Bala(pygame.sprite.Sprite):
+    def __init__(self, posicion) -> None:
+        super().__init__()
+        #creamos un rectangulo
+        self.image = pygame.Surface((5,10))
+        self.image.fill((255,0,0))
+        self.rect = self.image.get_rect()
+        self.rect.center = posicion
+
+    def update(self, *args: Any, **kwargs: Any) -> None:
+        self.rect.y -= 5
+
+    
--- python/spaceinvaders/space2.py
+++ python/spaceinvaders/space2.py
@@ -1,6 +1,7 @@
 import pygame
 import elementosSpace2
 import random
+import pygame_menu
 
 #inicializamos el juego
 pygame.init()
@@ -14,15 +15,17 @@ reloj = pygame.time.Clock()
 FPS = 60
 
 #booleano de control
-running = True
+running = [True]
 
-#crear la nave
+##crear la nave
 posicion = (200,200)
 nave = elementosSpace2.Nave(posicion)
 #nave = elementosspace.Nave((200,200))
 
 #crear un grupo de sprites
 grupo_sprites = pygame.sprite.Group(nave)
+fondo = elementosSpace2.Fondo()
+grupo_sprites.add(fondo)
 grupo_sprites.add(elementosSpace2.Nave((300,100)))
 grupo_sprites.add(elementosSpace2.Nave((400,100)))
 grupo_sprites.add(elementosSpace2.Nave((500,100)))
@@ -32,36 +35,77 @@ grupo_sprites.add(enemigo)
 
 #crear una variable que almacene la ultima vez que se creo un enemigo
 ultimo_enemigo_creado = 0
+frecuencia_creacion_enemigo = 2000
 
-#bucle principal
-while running:
-    #limitamos el bucle a framerate que hemos definido
-    reloj.tick(FPS)
+def set_difficulty(value, difficulty):
+    # Do the job here !
+    pass
+
+def start_the_game():
     
-    #gestionar la salida
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    #booleano de control
+    running = [True]
 
-    #creacion de enemigos
-    momento_actual = pygame.time.get_ticks()
-    if (momento_actual > ultimo_enemigo_creado + 60):
-        grupo_sprites.add(elementosSpace2.Enemigo((random.randint(0,pantalla.get_width()),0)))
-        ultimo_enemigo_creado = momento_actual
-
-    #fondo blanco
-    pantalla.fill((255,255,255))
+    global ultimo_enemigo_creado
+    global reloj
+    global frecuencia_creacion_enemigo
+    global FPS
+    global grupo_sprites_todos
+    global grupo_sprites_enemigos
+    global grupo_sprites_balas
     
-    teclas = pygame.key.get_pressed()
-    #segundo sprites
+    #bucle principal
+    while running[0]:
+        #limitamos el bucle a framerate que hemos definido
+        reloj.tick(FPS)
+    
+        #gestionar la salida
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        #creacion de enemigos
+        momento_actual = pygame.time.get_ticks()
+        if (momento_actual > ultimo_enemigo_creado + frecuencia_creacion_enemigo):
+            cordX = random.randint(0, pantalla.get_width())
+            cordY = -200
+            grupo_sprites.add(elementosSpace2.Enemigo((random.randint(0,pantalla.get_width()),0)))
+            ultimo_enemigo_creado = momento_actual
+
+        #capturamos las teclas
+        teclas = pygame.key.get_pressed()
+        if teclas[pygame.K_SPACE]:
+            nave.disparar(grupo_sprites)
+        
+        #capturamos las teclas
+        #teclas = pygame.key.get_pressed()
+        #if teclas[pygame.K_ESCAPE]:
+       #running[0] = False
+    
+        #fondo blanco
+        pantalla.fill((255,255,255))
+    
+        teclas = pygame.key.get_pressed()
+        #segundo sprites
     
 
-    #pintaremos
-    grupo_sprites.update(teclas)
-    grupo_sprites.draw(pantalla)
+        #pintaremos
+        grupo_sprites.update(teclas)
+        grupo_sprites.draw(pantalla)
 
-    #redibujar la pantalla
-    pygame.display.flip()
+        #redibujar la pantalla
+        pygame.display.flip()
+        
+
+menu = pygame_menu.Menu('Welcome', 400, 300,
+                       theme=pygame_menu.themes.THEME_BLUE)
+
+menu.add.text_input('Name :', default='John Doe')
+menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+menu.add.button('Play', start_the_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+
+menu.mainloop(pantalla)
 
 #finalizamos el juego
 pygame.quit()
